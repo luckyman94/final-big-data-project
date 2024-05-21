bc99656aafb422d329f5477780059cf20f1f2753
ilan <ilan.bacry20@gmail.com>
2024-05-21 21:13:26 +0200

    fix runtime

--- src/machine_learning/pipeline.py
+++ src/machine_learning/pipeline.py
@@ -4,17 +4,16 @@ from sklearn.pipeline import Pipeline
 
 from src.machine_learning.transformers.genre_renamer import GenreRenameTransformer
 from src.machine_learning.transformers.missing_values import MissingValuesTransformer
-from src.machine_learning.transformers.ohe import OneHotEncoderTransformer
 from src.machine_learning.transformers.runtime import RuntimeTransformer
-from src.machine_learning.transformers.tfidf import TFIDFTransformer
 
 
 class PipelineTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.steps = [
-            ('runtime', RuntimeTransformer()),
             ('missing_values', MissingValuesTransformer()),
+            ('runtime', RuntimeTransformer()),
             ('genre_renamer', GenreRenameTransformer()),
+
         ]
 
         self.pipeline = Pipeline(self.steps)
@@ -27,14 +26,11 @@ class PipelineTransformer(BaseEstimator, TransformerMixin):
 
 
 
-
-
-
 if __name__ == "__main__":
     a = pd.read_parquet("/Users/ilan/big-data-airflow-project/data/final_dataset.parquet")
     pp = PipelineTransformer()
     b = pp.fit_transform(a)
-    print(b)
+    print(b["Genre"].head())
 
 
 
--- src/machine_learning/transformers/runtime.py
+++ src/machine_learning/transformers/runtime.py
@@ -3,6 +3,7 @@ from sklearn.base import TransformerMixin, BaseEstimator
 
 DATA_DIR = "/Users/ilan/big-data-airflow-project/data"
 
+
 class RuntimeTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, column="Runtime"):
         self.column = column
@@ -14,25 +15,24 @@ class RuntimeTransformer(BaseEstimator, TransformerMixin):
 
         X_copy = X.copy()
 
-        X[self.column] = X[self.column].str.replace("min", "")
-        X[self.column] = X[self.column].str.replace("h", "")
-        X[self.column] = X[self.column].str.split(" ")
-
         def parse_runtime(runtime):
-            hours = int(runtime[0])
-            minutes = int(runtime[1]) if len(runtime) > 1 else 0
-            total_hours = hours + minutes / 60
-            if total_hours > 2:
-                return '> 2 hrs'
-            elif total_hours < 0.5:
-                return '< 30 minutes'
-            elif total_hours < 1 and total_hours >= 0.5:
-                return '30 - 60 mins'
-            else:
-                return '1-2 hour'
+            if 'h' in runtime:
+                hours, minutes = runtime.replace('min', '').split('h')
+                try:
+                    hours = int(hours.strip())
+                    minutes = int(minutes.strip())
+                    total_hours = hours + minutes / 60
+                    if total_hours > 2:
+                        return '> 2 hrs'
+                    elif total_hours < 0.5:
+                        return '< 30 minutes'
+                    elif total_hours < 1 and total_hours >= 0.5:
+                        return '30 - 60 mins'
+                    else:
+                        return '1-2 hours'
+                except ValueError:
+                    return runtime
+            return runtime
 
-        X[self.column] = X[self.column].apply(parse_runtime)
-        return pd.concat([X_copy, X[self.column]], axis=1)
-
-
-
+        X_copy[self.column] = X_copy[self.column].apply(parse_runtime)
+        return X_copy
