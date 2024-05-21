074619761855ae129f8dec5eee04efb08d3d59cd
ilan <ilan.bacry20@gmail.com>
2024-05-21 23:19:02 +0200

    end first_version of the model

--- src/machine_learning/model.py
+++ src/machine_learning/model.py
@@ -1,30 +1,31 @@
 import pandas as pd
 from sklearn.metrics.pairwise import linear_kernel
 
-from src.machine_learning.preprocessing import Preprocessing
-
+from src.machine_learning.pipeline import PipelineTransformer
 class RecommendationModel:
-    def __init__(self, df, tfidf_matrix):
-        self.df = df
-        self.tfidf_matrix = tfidf_matrix
+    def __init__(self, df):
+        self.pipeline = PipelineTransformer()
+        self.train = self.pipeline.fit_transform(df)
+        self.tfidf_matrix = self.pipeline.get_tfidf_matrix()
+        self.indices = pd.Series(range(len(self.train)), index=self.train['Title']).drop_duplicates()
         self.cosine_sim = linear_kernel(self.tfidf_matrix, self.tfidf_matrix)
-        self.indices = pd.Series(self.df.index, index=self.df['Title']).drop_duplicates()
 
     def get_recommendations(self, title):
-        print("Titles available in DataFrame:", self.df['Title'].unique())
         idx = self.indices.get(title, None)
         if idx is None:
             print("Title not found in the index.")
             return []
+
         sim_scores = list(enumerate(self.cosine_sim[idx]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
         movie_indices = [i[0] for i in sim_scores[1:11]]
-        return self.df['Title'].iloc[movie_indices]
 
-if __name__ == '__main__':
-    preprocessor = Preprocessing()
-    df_processed = preprocessor.apply_preprocessing()
-    tfidf_matrix = preprocessor.get_tfidf_matrix()
-    recommender = RecommendationModel(df_processed, tfidf_matrix)
-    recommendations = recommender.get_recommendations("Maverick")
-    print("Recommendations for 'Maverick':", recommendations)
+        if any(i >= len(self.train) for i in movie_indices):
+            print("Error: Some indices are out of bounds.")
+            return []
+
+        return self.train['Title'].iloc[movie_indices]
+
+
+
+
--- src/machine_learning/pipeline.py
+++ src/machine_learning/pipeline.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.pipeline import Pipeline
 
+
 from src.machine_learning.transformers.drop import DropColumn, DropDuplicates
 from src.machine_learning.transformers.filter_type import FilterTypeTransformer
 from src.machine_learning.transformers.genre import GenreTransformer, GenreRenameTransformer
@@ -14,15 +15,16 @@ from src.machine_learning.transformers.tfidf import TFIDFTransformer
 class PipelineTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.steps = [
+            ('drop_duplicated', DropDuplicates(columns=["Title"])),
             ('missing_values', MissingValuesTransformer()),
             ('runtime', RuntimeTransformer()),
             ('genre_renamer', GenreRenameTransformer()),
             ('genre', GenreTransformer()),
+            ('drop_columns', DropColumn(cols=["Genre"])),
             ('filter_type', FilterTypeTransformer()),
             ('ohe', OneHotEncoderTransformer(columns=["Runtime", "Type"])),
             ('tfidf', TFIDFTransformer(column="Summary", max_features=1000, n_components=50)),
-            ('drop_columns', DropColumn(cols=["Genre"])),
-            ('drop_duplicated', DropDuplicates()),
+
         ]
         self.pipeline = Pipeline(self.steps)
 
@@ -32,7 +34,8 @@ class PipelineTransformer(BaseEstimator, TransformerMixin):
     def transform(self, X):
         return self.pipeline.fit_transform(X)
 
-
+    def get_tfidf_matrix(self):
+        return self.pipeline.named_steps['tfidf'].get_tfidf_matrix()
 
 
 
